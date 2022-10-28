471cbc3635e1eb1531be6a447b2d739f23a297a8
MegaClinton <clinton3122003@gmail.com>
2022-10-28 14:31:23 -0500

    a

---
+++ main.cpp
@@ -0,0 +1,46 @@
+/* Authors: Clinton Nguyen (cdn0116), Manuel Osorto (meo0106), Aaron Millman (amm1076), Jaxon Olson (jco0120)
+   Instructor:  Shrestha
+   Description: CSCE 1030 Project 02
+*/
+
+#include "main.h"
+
+const int ROWS = 10;
+const int COLUMNS = 10;
+
+bool IsWrongFormat(string email)
+{
+
+}
+
+void intialize(int num = -1)
+{
+
+}
+
+bool checkRepeat(int array[][COLUMNS])
+{
+
+}
+
+void display()
+{
+
+}
+
+int main()
+{
+    int num_array[ROWS][COLUMNS];
+    int disp_array[ROWS][COLUMNS];
+
+    string email;
+    cout << "Enter your email: ";
+    cin >> email;
+    while(IsWrongFormat(email))
+    {
+
+    }
+
+
+    return 0;
+}
---
+++ main.h
@@ -0,0 +1,12 @@
+//
+// Created by clinton on 10/28/2022.
+//
+
+#ifndef PROJECT_02_CSCE_1030_MAIN_H
+#define PROJECT_02_CSCE_1030_MAIN_H
+
+#include <iostream>
+using namespace std;
+
+
+#endif //PROJECT_02_CSCE_1030_MAIN_H
