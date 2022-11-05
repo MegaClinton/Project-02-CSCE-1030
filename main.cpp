d84eec9b258ec994137402cc73e70478af2df393
MegaClinton <clinton3122003@gmail.com>
2022-11-05 16:30:29 -0500

    u

--- main.cpp
+++ main.cpp
@@ -43,11 +43,11 @@ bool checkRepeat(const int arr[][COLUMNS], int i, int j)
 
 }
 
-void display(const int arr[][COLUMNS])
+void display(const int arr[][COLUMNS], int userRow, int userCol)
 {
-   for(int i=0;i<ROWS;++i)
+   for(int i=0;i<userRow;++i)
     {
-        for( int j=0;j<COLUMNS;++j)
+        for( int j=0;j<userCol;++j)
         {
             cout<<arr[i][j]<<"\t";
         }
